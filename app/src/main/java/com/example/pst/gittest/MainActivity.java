5f493551c787c2f47f158689c1beafd3c55590b6
DAHAKA <dahaka68@gmail.com>
2017-03-08 15:32:37 +0300

    reformat code

--- app/src/main/java/com/example/pst/gittest/MainActivity.java
+++ app/src/main/java/com/example/pst/gittest/MainActivity.java
@@ -1,13 +1,80 @@
 package com.example.pst.gittest;
 
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends PreferenceActivity {
+
+    private static Preference.OnPreferenceChangeListener mListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if (preference instanceof SwitchPreference) {
+                SwitchPreference pref = (SwitchPreference) preference;
+                if (pref.isChecked()) {
+                    pref.setSummary("Включено");
+                } else {
+                    pref.setSummary("Выключено");
+                }
+            }
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.headers, target);
+    }
+
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName)
+                || SettingsFragment.class.getName().equals(fragmentName)
+                || GeneralPreferenceFragment.class.getName().equals(fragmentName)
+                || NotificationPreferenceFragment.class.getName().equals(fragmentName);
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+    public static class GeneralPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.general);
+
+            findPreference("general_switch1").setOnPreferenceChangeListener(mListener);
+        }
+    }
+
+    public static class NotificationPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.notification);
+        }
     }
 }
